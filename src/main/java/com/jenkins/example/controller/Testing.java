b8b641f3c2611e3b8e4662d309cd2af0f36ac319
sumit <sumitnanda502@gmail.com>
2025-08-21 21:47:10 +0530

    source commited for first commit

---
+++ src/main/java/com/jenkins/example/controller/Testing.java
@@ -0,0 +1,16 @@
+package com.jenkins.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Testing {
+	
+	
+	@GetMapping(value = "/welcome")
+	public String testing() {
+		
+		return "Working fine";
+	}
+
+}
