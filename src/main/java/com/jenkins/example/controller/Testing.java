1bab67ac71e6d49d9942dbee524cc792653330ba
sumit <sumitnanda502@gmail.com>
2025-08-27 12:13:05 +0530

    Docker jenkins integration 27-08

--- src/main/java/com/jenkins/example/controller/Testing.java
+++ src/main/java/com/jenkins/example/controller/Testing.java
@@ -10,7 +10,7 @@ public class Testing {
 	@GetMapping(value = "/welcome")
 	public String testing() {
 		
-		return "Working fine commit pipeline new docker";
+		return "Working fine commit pipeline new docker on 27-08";
 	}
 
 }
